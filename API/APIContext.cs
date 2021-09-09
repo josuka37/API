7bb6efdcc9e3a49c9f3e9fe91f7e01d4b59c3f74
Josué da Motta <josuka360@hotmail.com>
2021-09-09 20:03:26 -0300

    Criado as tabelas e conectado com banco de dados através do Entity Framework, Add-Migration e Update-DataBase estão funcionando

--- API/APIContext.cs
+++ API/APIContext.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using API.Domínio;
+using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -9,10 +10,13 @@ namespace API
     public class APIContext : DbContext
     {
        public DbSet<User> users { get; set; }
+        public DbSet<Medicamento> Medicamentos { get; set; }
+        public DbSet<Receita> Receitas { get; set; }
+        public DbSet<Pessoa> Pessoas { get; set; }
         public APIContext
             (DbContextOptions<APIContext>options) : base(options)
         {
-
+   
         }
     }
 }
---
+++ API/Migrations/20210909215452_v03.cs
@@ -0,0 +1,94 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+namespace API.Migrations
+{
+    public partial class v03 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Pessoas",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    Nome = table.Column<string>(type: "text", nullable: true),
+                    Cpf = table.Column<int>(type: "integer", nullable: false),
+                    Idade = table.Column<int>(type: "integer", nullable: false),
+                    Médico = table.Column<bool>(type: "boolean", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Pessoas", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Receitas",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    PessoaId = table.Column<int>(type: "integer", nullable: true),
+                    NomeMédico = table.Column<string>(type: "text", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Receitas", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Receitas_Pessoas_PessoaId",
+                        column: x => x.PessoaId,
+                        principalTable: "Pessoas",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Medicamentos",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    Nome = table.Column<string>(type: "text", nullable: true),
+                    NomeComercial = table.Column<string>(type: "text", nullable: true),
+                    PrincipioAtivo = table.Column<string>(type: "text", nullable: true),
+                    ConcentracaoMg = table.Column<double>(type: "double precision", nullable: false),
+                    HumanoOuPet = table.Column<bool>(type: "boolean", nullable: false),
+                    Restrito = table.Column<bool>(type: "boolean", nullable: false),
+                    ReceitaId = table.Column<int>(type: "integer", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Medicamentos", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Medicamentos_Receitas_ReceitaId",
+                        column: x => x.ReceitaId,
+                        principalTable: "Receitas",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Medicamentos_ReceitaId",
+                table: "Medicamentos",
+                column: "ReceitaId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Receitas_PessoaId",
+                table: "Receitas",
+                column: "PessoaId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Medicamentos");
+
+            migrationBuilder.DropTable(
+                name: "Receitas");
+
+            migrationBuilder.DropTable(
+                name: "Pessoas");
+        }
+    }
+}
